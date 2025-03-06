b4cadaa884e58919d0f1e4f01d49bd4123ad7900
vgabriel-pereira <gabrielptrindade02@gmail.com>
2025-03-06 20:06:16 -0300

    Alterando View Pratica 02

--- praticas/pratica02/App.js
+++ praticas/pratica02/App.js
@@ -4,31 +4,36 @@ import { StyleSheet, Text, View } from "react-native";
 function AppBar(props) {
   return (
     <View style={styles.appBar}>
-      <Text style={{fontSize: 20, fontWeight: "bold", color: "#fff"}}>{props.titulo}</Text>
+      <Text style={{ fontSize: 20, fontWeight: "bold", color: "#fff" }}>
+        {props.titulo}
+      </Text>
     </View>
   );
-  
 }
-
 
 function App() {
   return (
-    <View style={styles.container}>
+    <View >
       <AppBar titulo="Minha Aplicação React Native" />
-      <View style={{ flex: 1, backgroundColor: "#fff", justifyContent: "center", alignItems: "center" }}></View>
-    </View>  
+      <View style={styles.container}></View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {flex: 1, backgroundColor: "#fff", justifyContent: "center", alignItems: "center"},
-  appBar: {height: 50, backgroundColor: "#333", justifyContent: "center", paddingLeft: 16},
-  title: {fontSize: 20, fontWeight: "bold", color: "#fff"},
-
-
-
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  appBar: {
+    height: 50,
+    backgroundColor: "#333",
+    justifyContent: "center",
+    paddingLeft: 16,
+  },
+  title: { fontSize: 20, fontWeight: "bold", color: "#fff" },
 });
-
-
 
 export default App;
